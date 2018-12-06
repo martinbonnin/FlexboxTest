f62d35eb1c6faf247b35e0f35b59fe0a4ac32b5e
Martin Bonnin <martin.bonnin@dailymotion.com>
2018-12-06 11:42:24 +0100

    initial commit

---
+++ app/src/main/java/net/mbonnin/flexboxtest/MainActivity.kt
@@ -0,0 +1,34 @@
+package net.mbonnin.flexboxtest
+
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.flexbox.FlexboxLayoutManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = RecyclerView(this)
+        recyclerView.layoutManager =  FlexboxLayoutManager(this)
+        recyclerView.adapter = object: RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+                return object: RecyclerView.ViewHolder(TextView(this@MainActivity)){}
+            }
+
+            override fun getItemCount(): Int {
+                return 10000;
+            }
+
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                (holder.itemView as TextView).text = "position #$position"
+            }
+
+        }
+        recyclerView.smoothScrollToPosition(500)
+    }
+}
